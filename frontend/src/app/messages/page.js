acda6c1634dd95f99c5cfd9179051dc5dc2ef887
Bart710 <trsv420@gmail.com>
2024-09-03 11:54:29 -0500

    Update page.js

--- frontend/src/app/messages/page.js
+++ frontend/src/app/messages/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import AuthenticatedLayout from "@/components/AuthenticatedLayout";
-import Messages from "@/components/Messages";
+import Messages from "../../components/Messages";
 
 export default function MessagesPage() {
   const [userRole, setUserRole] = useState(null);
